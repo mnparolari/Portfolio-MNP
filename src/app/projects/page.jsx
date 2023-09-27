9f445f67b10fbab7a11c02267f3f0dc038d71623
Martin Parolari <mnparolari@gmail.com>
2023-09-27 12:00:32 -0300

    MNP-Sept2023

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -2,7 +2,7 @@ import StyleProyects from './projects.module.css'
 import LikeAtHome from './LikeAtHome'
 import Presente from './Presente'
 import Rpm from './Rpm'
-import EnrollmentManager from './enrollmentManager'
+import EnrollmentManager from './EnrollmentManager'
 
 export default function Projectos() {
   return (
